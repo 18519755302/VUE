4e3267a0e1677c9a32c34910345cb58a0cfa8031
18519755302 <869028585@qq.com>
2020-11-23 22:58:45 +0800

    vue59

---
+++ router/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <router-link to="/">demo1</router-link>
+      <router-link to="/demo2">demo2</router-link>
+      <router-link to="/demo3">demo3</router-link>
+    </div>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+.link-exact-active {
+  color: cadetblue;
+}
+</style>
