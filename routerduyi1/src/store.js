0d14ee4828a255abb321a13a70eaf903daa8ce49
18519755302 <869028585@qq.com>
2020-12-10 20:12:16 +0800

    vue70

--- routerduyi1/src/store.js
+++ routerduyi1/src/store.js
@@ -1,14 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {
+    COUNT_PLUS
+} from './store/mutation-types';
 
 //vue使用Vuex
 Vue.use(Vuex);
 
 //注册Store选项，并导出
 export default new Vuex.Store({
+    strict: process.env.NODE_ENV !== 'production',
     state: {
         count: 0,
-        studentList: []
+        studentList: [],
+        obj: {
+            a: 1
+        },
+        msg: ''
     },
     getters: {
         studentsLength(state) {
@@ -24,5 +32,25 @@ export default new Vuex.Store({
             })
         }
 
+    },
+    mutations: {
+        countPlus(state) {
+            state.count++;
+        },
+        countPlusTwo(state) {
+            state.count += 2;
+        },
+        countPlusN(state, n) {
+            state.count += n;
+        },
+        countPlusPayload(state, payload) {
+            state.count += payload.n;
+        },
+        [COUNT_PLUS](state) {
+            state.count++;
+        },
+        input(state, value) {
+            state.msg = value;
+        }
     }
 });
--- routerduyi1/src/views/BaseDemo1.vue
+++ routerduyi1/src/views/BaseDemo1.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     demo1
-    <button @click="$store.state.count++">+1</button>
+    <button @click="plus">+1</button>
+    <button @click="countPlusTwo">+2</button>
+    <button @click="plusTen">+10</button>
+    <button @click="changeObj">changeObj</button>
     <hr />
     {{ storeCount }}
-
     {{ addCount(3) }}
+    {{ this.obj }}
+    <!-- <hr />
+    <input type="text" :value="msg" @input="input" />
+    {{ msg }} -->
+    <hr />
+    <input type="text" v-model="msg" />
+    {{ msg }}
     <div class="buttom">
       <router-link to="/demo2">demo2</router-link>
       <router-link to="/demo2#a">此处可跳到demo2的a</router-link>
@@ -13,29 +22,57 @@
   </div>
 </template>
 <script>
-//获取mapState，以便从中找到count值
-import { mapState, mapGetters } from "vuex";
+//获取mapMutations
+import { mapState, mapGetters, mapMutations } from "vuex";
+import { COUNT_PLUS } from "../store/mutation-types";
 export default {
-  mounted() {
-    console.log(mapGetters(["addCount"]));
-  },
-  //分享名和自己起的名相同，可这样写
-  //computed: mapState(["count"]),
-  //也可以这样写
-  // computed: {
-  //   ...mapState(["count"]),
-  // },
-  //如果分享名和自己起的名不相同
+  mounted() {},
   computed: {
     ...mapState({
-      //自己起的名：函数
-      //storeCount: (state) => state.count,
       //语法糖 自己起的名字："分享名"
       storeCount: "count",
+      //用于验证
+      obj: "obj",
+      //msg: "msg",
     }),
     ...mapGetters({
       addCount: "addCount",
     }),
+    msg: {
+      get() {
+        return this.$store.state.msg;
+      },
+      set(value) {
+        this.$store.commit("input", value);
+      },
+    },
+  },
+  methods: {
+    //多个方法可以在数组里加入，也可以单独写一个mapMutations放入方法名
+    ...mapMutations(["countPlus", "countPlusTwo"]),
+    // ...mapMutations(["countPlusTwo"]),
+    //也可单独写法方法，放入vuex定义好的Mutation名
+    plus() {
+      //this.$store.commit("countPlus");
+      this.$store.commit(COUNT_PLUS);
+    },
+    //Mutation中方法加参数
+    plusTen() {
+      //第①种方法
+      //this.$store.commit("countPlusN", 10);
+      //第②种方法
+      //this.$store.commit("countPlusPayload", { n: 10 });
+      this.$store.commit({
+        type: "countPlusPayload",
+        n: 10,
+      });
+    },
+    changeObj() {
+      this.$store.state.obj = { ...this.$store.state.obj, b: 123 };
+    },
+    input(e) {
+      this.$store.commit("input", e.target.value);
+    },
   },
 };
 </script>
